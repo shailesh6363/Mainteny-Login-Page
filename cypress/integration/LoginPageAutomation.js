ff4bc8252dacb6a24848501d92245e338cdf139b
Shailesh Waghole <wagholeshailesh6363@gmail.com>
2022-02-05 16:10:02 +0530

    Added Test Case TC_Login_003
    Added Test Case TC_Login_003_To verify Login Page is displayed after selecting Login option from Mainteny home page navigation bar

--- cypress/integration/LoginPageAutomation.js
+++ cypress/integration/LoginPageAutomation.js
@@ -1,8 +1,10 @@
 /// <reference types="Cypress" />
 import LoginPage from '../pageObjects/loginPage'
+import HomePage from'../pageObjects/homePage'
 
 describe('Functional Test Cases',function(){
-
+        const logos=new LoginPage()
+        const home=new HomePage()
     it("TC_Login_001_To verify Login Page is displayed",function(){
 
         cy.visit(Cypress.env('url'))
@@ -11,10 +13,21 @@ describe('Functional Test Cases',function(){
 
     it("TC_Login_002_To verify Maintany home page is launched after selecting Maintany logo from login page navigation bar",function(){
 
-        const logos=new LoginPage()
+       
         cy.visit(Cypress.env('url'))
         logos.selectLogo().click()
         cy.title().should('include',"Empowering service technicians")
+    })
+
+    it("TC_Login_003_To verify Login Page is displayed after selecting Login option from Maintany home page navigation bar",function(){
+
+        
+        cy.visit(Cypress.env('url'))
+        logos.selectLogo().click()
+        cy.title().should('include',"Empowering service technicians")
+        home.selectLogin().click()
+        cy.url().should('include','login')
+
     })
 
 
